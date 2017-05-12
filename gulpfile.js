7a60af5fbc301793cda61aad2bdf42e0ee236d03
Arnab Banerji <reach.arnab@live.in>
2017-05-13 03:08:30 +0530

    Gulp modified to delete pkg folder before packing.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var JSDOM = require('jsdom').JSDOM;
+var clean = require('gulp-clean');
 var dom;
 
 var tagsToSearch = [
@@ -38,66 +39,72 @@ var foldersToCopy = [
     }
 ];
 
-JSDOM.fromFile(src + devPage).then(function (d) {
-    dom = d;
-    getDevFiles();
-});
+gulp.task('default', function () {
+    // place code for your default task here
 
-function getDevFiles() {
+    gulp.src('./pkg/', {read: false})
+        .pipe(clean());
 
 
-    for (var i = 0; i < tagsToSearch.length; i++) {
-        var tag = tagsToSearch[i];
-        var tagName = tag.name;
-        var tagFiles = tag.files;
-        var tags = dom.window.document.querySelectorAll(tagName);
+    JSDOM.fromFile(src + devPage).then(function (d) {
+        dom = d;
+        getDevFiles();
+    });
 
-        for (var j = 0; j < tags.length; j++) {
-            var script = tags[j];
-            var file = src + script.getAttribute(tag.attr);
-            script.outerHTML = '';
-            tagFiles.push(file);
-            console.log('%s#%d=%s', tagName, j, file);
-        }
-        var newTag = dom.window.document.createElement(tagName);
-        newTag.setAttribute(tag.attr, tag.concatFile);
+    function getDevFiles() {
 
-        if (tag.additionalAttr) {
-            for (var k in tag.additionalAttr) {
-                newTag.setAttribute(k, tag.additionalAttr[k]);
+
+        for (var i = 0; i < tagsToSearch.length; i++) {
+            var tag = tagsToSearch[i];
+            var tagName = tag.name;
+            var tagFiles = tag.files;
+            var tags = dom.window.document.querySelectorAll(tagName);
+
+            for (var j = 0; j < tags.length; j++) {
+                var script = tags[j];
+                var file = src + script.getAttribute(tag.attr);
+                script.outerHTML = '';
+                tagFiles.push(file);
+                console.log('%s#%d=%s', tagName, j, file);
+            }
+            var newTag = dom.window.document.createElement(tagName);
+            newTag.setAttribute(tag.attr, tag.concatFile);
+
+            if (tag.additionalAttr) {
+                for (var k in tag.additionalAttr) {
+                    newTag.setAttribute(k, tag.additionalAttr[k]);
+                }
+
+            }
+
+            var appendTo = dom.window.document.querySelectorAll(tag.appendTo)[0];
+            appendTo.appendChild(newTag);
+
+            var stream = gulp.src(tagFiles)
+                .pipe(concat(tag.concatFile));
+
+            if (tag.postConcatTask) {
+                stream.pipe(tag.postConcatTask()).pipe(gulp.dest(pkg));
+            } else {
+                stream.pipe(gulp.dest(pkg));
             }
 
         }
 
-        var appendTo = dom.window.document.querySelectorAll(tag.appendTo)[0];
-        appendTo.appendChild(newTag);
+        fs.writeFile(pkg + pkgPage, dom.window.document.documentElement.outerHTML, function (error) {
+            if (!error) {
+                console.log('Package HTML ready');
+            }
+        });
 
-        var stream = gulp.src(tagFiles)
-            .pipe(concat(tag.concatFile));
-
-        if (tag.postConcatTask) {
-            stream.pipe(tag.postConcatTask()).pipe(gulp.dest(pkg));
-        } else {
-            stream.pipe(gulp.dest(pkg));
+        for (var i = 0; i < foldersToCopy.length; i++) {
+            var folder = foldersToCopy[i];
+            var folderSrcPath = src + folder.src + folder.name + '/**/*';
+            var folderDestPath = pkg + folder.name + '/';
+            gulp.src([folderSrcPath]).pipe(gulp.dest(folderDestPath));
         }
 
     }
 
-    fs.writeFile(pkg + pkgPage, dom.window.document.documentElement.outerHTML, function (error) {
-        if (!error) {
-            console.log('Package HTML ready');
-        }
-    });
 
-    for (var i = 0; i < foldersToCopy.length; i++) {
-        var folder = foldersToCopy[i];
-        var folderSrcPath = src + folder.src + folder.name + '/**/*';
-        var folderDestPath = pkg + folder.name + '/';
-        gulp.src([folderSrcPath]).pipe(gulp.dest(folderDestPath));
-    }
-
-}
-
-gulp.task('default', function () {
-    // place code for your default task here
 });
